45646d541d0a69162ba4a4fbcabf9c079b289e4b
JRealMe <jeremyloo82@yahoo.com>
2022-12-25 21:49:23 +0800

    Updates

---
+++ precache-manifest.cbf8bb77930af0f3dddc786d22e27c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f670373b4b321d25b8bc",
+    "url": "/JBank-Dapp/static/css/main.ae58608f.chunk.css"
+  },
+  {
+    "revision": "f670373b4b321d25b8bc",
+    "url": "/JBank-Dapp/static/js/main.ea59eeaf.chunk.js"
+  },
+  {
+    "revision": "9b5baa598746dc6139d0",
+    "url": "/JBank-Dapp/static/js/runtime~main.5b300eb2.js"
+  },
+  {
+    "revision": "342bbf72a00637c12c50",
+    "url": "/JBank-Dapp/static/js/2.4f27a283.chunk.js"
+  },
+  {
+    "revision": "b8002e3123a0f2bc517b",
+    "url": "/JBank-Dapp/static/js/3.240d59fc.chunk.js"
+  },
+  {
+    "revision": "2f5a733de2b46f49f4b6939963b727fc",
+    "url": "/JBank-Dapp/index.html"
+  }
+];
